cdd3b5fec7eaf4dd2f6a2a3feacd2a9ded90abcc
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-01 11:21:34 +0530

    sai

--- linked-list/Doubly-Linked-List.c
+++ linked-list/Doubly-Linked-List.c
@@ -45,16 +45,41 @@ void insert_pos() {
 		printf("enter the position : ");
 		scanf("%d",&pos);
 			if( pos > length() ) {
-				printf("enter teh 
-
-		
-
-
-
+				printf("enter the valid position\n");
+				}
+				else if( pos == 1 ) {
+  				     insert_begin();
+				}
+				else {
+				struct node *p = root
+				     int k = 0;
+				  while( k < pos ) {
+					p = p->next;
+					k++;
+					}
+				
 		struct node *newnode;
 		newnode = ( *struct node )malloc( sizeof( struct node ) );
 		printf("enter the data: ");
 		scanf("%d",&newnode->data);
-		newnode->prev = NULL;
-		newnode->next = NULL;
-		
+		newnode->prev = p;
+		newnode->next = p->next;
+		p->next       = newnode;
+		newnode->next->prev = newnode;
+		}
+	}
+	
+void delete_begin() {
+	 struct node *temp;
+	 if( head == NULL ) {
+		printf("List is empty\n");
+		}
+		else {
+		      temp 	 = head;
+		      head 	 = head->next;
+		      head->prev = NULL;
+		      free(temp);
+		}	
+	}
+void delete_last() {
+	
