333fddc45f814596194273b0c917163474c54391
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-08 14:58:28 +0530

    sai update

--- linked-list/Circular-linked-list.c
+++ linked-list/Circular-linked-list.c
@@ -40,13 +40,33 @@ void append() {
                         }
                 }
 
+int length() {
+	struct node *temp = tail;
+		int len = 0;
+		while( temp != NULL ) {
+			len++;
+			temp = temp->next;
+			}
+		return len;
+	}
+
+					
 void insert_pos() {
-		struct node *newnode;
-		newnode = ( struct node* )malloc( sizeof(struct node ));
-        printf("enter the data\n");
-        scanf("%d",&newnode->data );
-        newnode->next = NULL;
-                if( last == NULL ) {
-                        last       = newnode;
-                        last->next = newnode;
-                        }
+		int pos;
+		printf("Enter the position: ");	
+		scanf("%d",&pos);	
+			if( pos > len || pos < 0 ) {
+				printf("enter the valid input\n");
+				}
+			   else {
+				struct node *temp = tail;
+			            int k  = 1;
+				    while( k < pos - 1 ) {
+						temp = temp->next;
+						}
+				    newnode->next = temp->next;
+				    temp->next    = newnode;
+				}
+			}
+		
+							
