b335fd9029c04ceb819508b45f1d9f9ba8d81ad3
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-14 19:51:25 +0530

    update from sai

--- Queue/circular-queue.c
+++ Queue/circular-queue.c
@@ -5,7 +5,7 @@ int queue[N];
 int front = -1;
 int rear = -1;
 
-void enqueue_rear() {
+void enqueue() {
 	if( (rear + 1 ) % N == front ) {
 		printf("Over Flow\n ");
 		}
@@ -26,29 +26,7 @@ void enqueue_rear() {
 		}
 	}
 }
-
-void enqueue_front() {
-	if( (rear + 1 ) % N == front ) {
-		printf("Over Flow\n ");
-		}
-		   else {
-			int k;
-                        printf("enter the number to insert: ");
-                        scanf("%d",&k);
-				if( front == -1 ) {
-					rear = front = 0;
-					queue[rear] = k;
-					printf("element %d is inserted\n" , k);
-					}
-			else {
-				front = ( front + 1 % N );
-				queue[rear] = k;
-				printf("element %d is inserted\n" , k);
-				
-		}
-	}
-}
-void dequeue_front() {
+void dequeue() {
 		if( rear == -1  || front > rear ) {
                 	printf("Under Flow\n ");
                 	}
@@ -60,19 +38,6 @@ void dequeue_front() {
 		      front = ( front + 1 ) % N;
 			}
 	}
-	void dequeue_rear() {
-		if( rear == -1  || front > rear ) {
-                	printf("Under Flow\n ");
-                	}
-		else if( front == rear ) {
-			front = rear = -1;
-			}
-		 else {
-		      printf("element %d is deleted\n",queue[rear]);
-		      rear = ( rear + 1 ) % N;
-			}
-	}
-
 void display() {
 		if( rear == -1 || front > rear )
                 	printf("Under Flow\n");
@@ -90,26 +55,19 @@ void display() {
 int main() {
 	 while(1) {
                 printf("---- Choose the option ----\n");
-                printf("----     1.Enqueue_fornt  ----\n");
-		printf("----     2.Enqueue_rear   ----\n");
-                printf("----     3.Dequeue_front  ----\n");
-		printf("----     4.Dequeue_rear  ----\n");
-                printf("----     5.Display     ----\n");
+                printf("----     1.Enqueue     ----\n");
+                printf("----     2.Dequeue     ----\n");
+                printf("----     3.Display     ----\n");
                 printf("----     0.Exit        ----\n");
 
                 int ch;
                 scanf("%d",&ch);
                 switch( ch ) {
-                        case 1 :  enqueue_front();
+                        case 1 :  enqueue();
                                   break;
-			case 2 :  enqueue_rear();
+                        case 2 :  dequeue();
                                   break;
-                        case 3 :  dequeue_front();
-                                  break;
-
-			case 4 :  dequeue_rear();
-                                  break;
-                        case 5 :  display();
+                        case 3 :  display();
                                   break;
                         case 0 :  exit(0);
                                   break;
@@ -119,5 +77,4 @@ int main() {
         return 0;
 }
 			
-
 
