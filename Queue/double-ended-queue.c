c635734b2e8b7f318158ce1f016c9562ff8484a8
saitadikonda99 <saiitadikonda7@gmail.com>
2023-02-15 13:39:27 +0530

    update from sai

---
+++ Queue/double-ended-queue.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define Max 5
+
+int front = -1;
+int rear  = -1;
+	
+void insert_front() {
+		
+	
+	
+	
+
+		}
+	
+void delete_last() {
+	
+	
+
+	
+	
+
+	
+		}
+	
+
+void insert_begin() {
+	
+	
+	
+
+	
+	
+	
+		}
+	
+void delete_last() {
+	
+	
+		
+
+	
+
+	
+
+
+	
+		}
+
+void display() {
+	
+	
+
+
+	
+		}
+
+int main() {
+		
+	while(1)  {
+
+		  
